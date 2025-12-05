b4e705920514e451cd2b689dd9cbfec5d5adc22e
JovinMtl <nsanzumukizath@gmail.com>
2025-12-05 11:32:58 +0200

    created a custom API for news app.

--- news/models.py
+++ news/models.py
@@ -1,17 +1,24 @@
 from django.db import models
 
 from wagtail.models import Page
-from wagtail.fields import RichTextField
+from wagtail.fields import RichTextField, StreamField
 from wagtail.api import APIField
 from wagtail.images.api.fields import ImageRenditionField
 from wagtail.admin.panels import FieldPanel
+from wagtail import blocks
+from wagtail.images.blocks import ImageChooserBlock
 # from wagtail.admin.edit
 
 # Create your models here.
 
 class NewsPage(Page):
     intro = models.CharField(max_length=250)
-    body  = RichTextField(blank=True)
+    # body  = RichTextField(blank=True)
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname="full title", icon="title")),
+        ("paragraph", blocks.RichTextBlock(icon="pilcrow")),
+        ("image", ImageChooserBlock(icon="image"))
+    ])
     image = models.ForeignKey(
         'wagtailimages.Image', 
         null=True, 
