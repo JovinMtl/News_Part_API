1ffbcbbef2ddcc6c6edcd4035e30db941a362f2a
JovinMtl <nsanzumukizath@gmail.com>
2025-12-05 11:51:25 +0200

    created an endpoint file for the custom api.

--- customAPI/views.py
+++ customAPI/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
+
+from news.models import NewsPage
 # Create your views here.
+
+
+from .serializers import NewsSerializer
+
+
+
+class NewsOps(viewsets.ViewSet):
+    """
+        Will do custom GET queries on News model.
+    """
+    @action(methods=['get'], detail=True)
+    def get_pages(self, request):
+        """
+        should return all pages.
+        """
+        pages = NewsPage.objects.all()
+        pages_serialized = NewsSerializer(pages, many=True)
+
+        if pages_serialized.is_valid:
+            return Response(pages_serialized.data)
+        
+        return Response({"error": "Could not serialize data"})
