607d97ff9e55ab47bd51d00068c9a23aa66eb662
JovinMtl <nsanzumukizath@gmail.com>
2026-01-14 19:31:25 +0200

    removed  session cookies to fully cached the api path

--- news/middleware.py
+++ news/middleware.py
@@ -9,6 +9,7 @@ class PublicAPICacheHeadersMiddleware(MiddlewareMixin):
         path = request.path
         if path.startswith("/api/"):
             response["Cache-Control"] = "public, max-age=300, s-maxage=300"
+            response.cookies.clear()  # Remove session cookies so CF will cache
         elif path.startswith("/media/"):
             response["Cache-Control"] = "public, max-age=86400, s-maxage=86400"  # 1 day
         return response
