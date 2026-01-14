0bd9371d13f7a7f62305f7483d16270909b05f3e
JovinMtl <nsanzumukizath@gmail.com>
2026-01-14 11:03:10 +0200

    fixed the broken migrations

--- news/blocks.py
+++ news/blocks.py
@@ -1,27 +1,9 @@
 from wagtail import blocks
 from wagtail.images.blocks import ImageChooserBlock
 from wagtail.images.api.fields import ImageRenditionField
-from wagtail.api import APIField
-
 
 class ResponsiveImageBlock(blocks.StructBlock):
     image = ImageChooserBlock(required=True)
-
-    # api_fields = [
-    #     APIField(
-    #         "small",
-    #         # serializer=ImageRenditionField("fill-400x300", source="image"),
-    #         serializer=ImageRenditionField("width-350", source="image"),
-    #     ),
-    #     APIField(
-    #         "medium",
-    #         serializer=ImageRenditionField("width-780", source="image"),
-    #     ),
-    #     APIField(
-    #         "large",
-    #         serializer=ImageRenditionField("width-1600", source="image"),
-    #     ),
-    # ]
 
     def get_api_representation(self, value, context=None):
         image = value["image"]
@@ -32,24 +14,25 @@ class ResponsiveImageBlock(blocks.StructBlock):
                 "title": image.title,
             },
             "small": ImageRenditionField(
-                "fill-400x300|format-avif",
-                source="image"
+                "fill-400x300|format-avif", source="image"
             ).to_representation(image),
             "medium": ImageRenditionField(
-                "fill-800x450|format-avif",
-                source="image"
+                "fill-800x450|format-avif", source="image"
             ).to_representation(image),
             "large": ImageRenditionField(
-                "fill-1080x720|format-avif",
-                source="image"
+                "fill-1080x720|format-avif", source="image"
             ).to_representation(image),
             "xlarge": ImageRenditionField(
-                "fill-1600x900|format-avif",
-                source="image"
+                "fill-1600x900|format-avif", source="image"
             ).to_representation(image),
         }
 
+    class Meta:
+        icon = "image"
+        label = "Responsive image"
 
-    # class Meta:
-    #     icon = "image"
-    #     label = "Responsive image"
+
+class NewsBodyBlock(blocks.StreamBlock):
+    heading = blocks.CharBlock(classname="full title", icon="title")
+    paragraph = blocks.RichTextBlock(icon="pilcrow")
+    image = ResponsiveImageBlock()
--- news/models.py
+++ news/models.py
@@ -1,40 +1,41 @@
 from django.db import models
-
 from wagtail.models import Page
 from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel
 from wagtail.api import APIField
 from wagtail.images.api.fields import ImageRenditionField
-from wagtail.admin.panels import FieldPanel
-from wagtail import blocks
-# from wagtail.admin.edit
 
-from .blocks import ResponsiveImageBlock
-
-# Create your models here.
+from .blocks import NewsBodyBlock
 
 class NewsPage(Page):
     image = models.ForeignKey(
-        'wagtailimages.Image', 
-        null=True, 
+        "wagtailimages.Image",
+        null=True,
         blank=True,
-        on_delete=models.SET_NULL
-        )
+        on_delete=models.SET_NULL,
+    )
     intro = models.CharField(max_length=250)
-    # body  = RichTextField(blank=True)
-    body = StreamField([
-        ('heading', blocks.CharBlock(classname="full title", icon="title")),
-        ("paragraph", blocks.RichTextBlock(icon="pilcrow")),
-        ("image", ResponsiveImageBlock(icon="image"))
-        ], blank=True, use_json_field=True)
 
-    content_panels = Page.content_panels  + [
+    body = StreamField(
+        NewsBodyBlock(),
+        blank=True,
+        use_json_field=True,
+    )
+
+    content_panels = Page.content_panels + [
+        FieldPanel("image"),
         FieldPanel("intro"),
         FieldPanel("body"),
-        FieldPanel("image")
     ]
 
     api_fields = [
-        APIField('intro'),
-        APIField('body'),
-        APIField('image_thumbnail', serializer=ImageRenditionField('width-400|format-avif', source='image')),
+        APIField("intro"),
+        APIField("body"),
+        APIField(
+            "image_thumbnail",
+            serializer=ImageRenditionField(
+                "width-400|format-avif",
+                source="image"
+            ),
+        ),
     ]
